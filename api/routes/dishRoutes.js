ff1001bfb5af3bde1cb4ff2803ca484fb7ca4b0e
Ivana Huckova <ivana.huckova@gmail.com>
2019-03-28 15:20:43 +0100

    Create post new dish functionality

--- api/routes/dishRoutes.js
+++ api/routes/dishRoutes.js
@@ -1,4 +1,4 @@
-/*- `getDishes()`: should return a list of all dishes in the database.
+/*
 - `addDish(dish)`: should add the **dish** to the database and return the `id` of the new **dish**.
 - `getDish(id)`: should return the **dish** with the provided `id` and include a list of the related recipes. */
 
@@ -16,6 +16,16 @@ routes.get('/', async (req, res) => {
   try {
     const allDishes = await db('dishes');
     res.status(200).json(allDishes);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// ========== POST ROUTES ========== //
+routes.post('/', async (req, res) => {
+  try {
+    const newDish = await db('dishes').insert({ dish_name: req.body.dish_name });
+    res.status(201).json(newDish[0]);
   } catch (error) {
     res.status(500).json(error);
   }
